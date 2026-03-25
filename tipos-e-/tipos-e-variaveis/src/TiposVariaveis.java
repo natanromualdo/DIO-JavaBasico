2295f341c5ba3a6daf8f2b23aeef64613624f221
Natan Romualdo <natancontato2001@gmail.com>
2026-03-25 00:50:05 -0300

    Criando projeto para exempli de tipos e variaveis

--- tipos-e-/tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-/tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        
+        // Tipos primitivos
+        byte idadeByte = 30; // Byte
+        short idadeShort = 30; // Short
+        int idadeInteiro = 30; // Inteiro
+        long salarioLong = 50000L; // Long
+        Float salarioFloat = 2500.50f; // Ponto flutuante
+        double salarioDouble = 2500.50; // Ponto flutuante
+        char genero = 'M'; // Caractere
+        boolean isAtivo = true; // Booleano
+
+        // Exibindo os valores
+        System.out.println("Idade (Byte).....: " + idadeByte);
+        System.out.println("Idade (Short)....: " + idadeShort);
+        System.out.println("Idade (Inteiro)..: " + idadeInteiro);
+        System.out.println("Salário (Long)...: " + salarioLong);
+        System.out.println("Salário (Float)..: " + salarioFloat);
+        System.out.println("Salário (Double).: " + salarioDouble);
+        System.out.println("Gênero...........: " + genero);
+        System.out.println("Ativo............: " + isAtivo);
     }
 }
